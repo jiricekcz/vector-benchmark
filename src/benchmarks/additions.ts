5275eaea8d7803dcd977cd97fe3c87c869a443b7
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-02-11 10:12:22 +0100

    fisrt testing benchmark

--- src/benchmarks/additions.ts
+++ src/benchmarks/additions.ts
@@ -1,12 +1,23 @@
 import { Benchmark, BenchmarkOptions } from '../types';
+import VectOps from 'vect-ops';
+import { performance } from 'perf_hooks';
 export const ADDITION_IN_PLACE: Benchmark<BenchmarkOptions['addition-in-place']> = {
     name: 'addition-in-place',
     fullName: ({ vectorLength, rounds }) => {
         return `Addition of ${vectorLength}-dimensional vectors in place ${rounds} times`
     },
+    runLength: ({ rounds, vectorLength }) => rounds * vectorLength,
     benchmarks: {
         'vect-ops': ({ vectorLength, rounds }) => {
-            return null;
+            const vect1 = new Array(vectorLength).fill(0);
+            const vect2 = new Array(vectorLength).fill(1);
+
+            const start = performance.now();
+            for (let i = 0; i < rounds; i++) {
+                VectOps.add(vect1, vect2);
+            }
+            const end = performance.now();
+            return end - start;
         }
     }
 };
--- src/types.ts
+++ src/types.ts
@@ -3,9 +3,23 @@ export const LIBRARY_NAMES = ["vect-ops"] as const;
 export type LibraryName = typeof LIBRARY_NAMES[number];
 
 export type Benchmark<A extends Record<string, any>> = {
+    /** Name of the benchmark */
     name: string,
+    /** Full name of the benchmark, dependant on the options */
     fullName: (options: A) => string,
+    /** An approximate number of operations to be performed */
+    runLength: (options: A) => number,
+    /** 
+     * The actual benchmarks  
+     * The key is the name of the library  
+     * If a benchmark returns null, it is assumed that the library does not support the benchmark
+     * 
+     */
     benchmarks: {
+        /**
+         * @param options The options for the benchmark
+         * @returns A number representing the milliseconds it took to run the benchmark, or null if the library does not support the benchmark
+         */
         [K in LibraryName]: (options: A) => number | Promise<number> | null;
     }
 }
